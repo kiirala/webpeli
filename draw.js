baa41146c4e5c6616c4cc990b434b1448221d7d9
kiirala <kiirala@15d524df-0ca0-45ad-9872-4ba5b019094f>
2011-02-23 10:13:48 +0000

    Webpeliin vihollinen ja piirroksia
    git-svn-id: svn+ssh://melkki/fs-1/1/kiirala/repo-svn/webpeli@98 15d524df-0ca0-45ad-9872-4ba5b019094f

--- draw.js
+++ draw.js
@@ -3,33 +3,57 @@ const FPS = 30;
 var x = 0;
 var y = 0;
 var time = 0;
-var aspectRatio = 4/3;
+var aspectRatio = 3/4;
 var scale = 1;
-var nominalWidth = 1024;
+var nominalWidth = 512;
 var nominalHeight = nominalWidth / aspectRatio;
 var image = new Image();
 image.src = "gears.png";
 var canvas = null;
 var context2D = null;
 var player = null;
+var bullets = [];
 
-function Player() {
+function Sprite(image) {
     this.x = 0;
     this.y = 0;
-    this.image = new Image();
-    this.image.src = "character.png";
-    this.minX = function() {
-	return this.x - this.image.width / 2;
-    };
-    this.minY = function() {
-	return this.y - this.image.height / 2;
-    };
-    this.maxX = function() {
-	return this.x + this.image.width / 2;
-    };
-    this.maxY = function() {
-	return this.y + this.image.height / 2;
-    };
+    this.image = image;
+}
+Sprite.prototype.minX = function() {
+    return this.x - this.image.width / 2;
+};
+Sprite.prototype.minY = function() {
+    return this.y - this.image.height / 2;
+};
+Sprite.prototype.maxX = function() {
+    return this.x + this.image.width / 2;
+};
+Sprite.prototype.maxY = function() {
+    return this.y + this.image.height / 2;
+};
+
+function Bullet(image, x, y, speedx, speedy, accelx, accely) {
+    this.x = x;
+    this.y = y;
+    this.speedx = speedx;
+    this.speedy = speedy;
+    this.accelx = accelx;
+    this.accely = accely;
+    this.image = image;
+}
+Bullet.prototype = new Sprite();
+Bullet.prototype.move = function(time) {
+    this.speedx += this.accelx * time;
+    this.speedy += this.accely * time;
+    this.x += this.speedx * time;
+    this.y += this.speedy * time;
+}
+Bullet.prototype.isDead = function() {
+    if (this.maxX() < x || this.minX() >= x + nominalWidth ||
+	this.maxY() < y || this.maxY() >= y + nominalHeight)
+	return true;
+    else
+	return false;
 }
 
 function getWindowSize() {
@@ -64,17 +88,27 @@ function resizeWindow() {
 }
 
 $(function() {
-      canvas = document.getElementById('canvas');
-      context2D = canvas.getContext('2d');
-      setInterval(draw, 1000 / FPS);
-      resizeWindow();
-      player = new Player();
-      player.x = player.image.width / 2;
-      player.y = nominalHeight / 2;
-      document.onkeydown = keydown;
-      document.onkeyup = keyup;
-  });
-
+    canvas = document.getElementById('canvas');
+    context2D = canvas.getContext('2d');
+    setInterval(draw, 1000 / FPS);
+    resizeWindow();
+    var plimg = new Image();
+    plimg.src = "character.png"
+    $(plimg).ready(function() {
+	player = new Sprite(plimg);
+	player.x = nominalWidth / 2;
+	player.y = player.image.height / 2;
+	document.onkeydown = keydown;
+	document.onkeyup = keyup;
+    });
+    redbullet = new Image();
+    redbullet.src = "redbullet.png";
+    var now = new Date();
+    startTime = now.getTime() / 1000;
+    startAlpha = 0;
+    alive = true;
+});
+    
 $(window).resize(
 function() {
     resizeWindow();
@@ -128,33 +162,88 @@ function keyup(event) {
 //);
 
 function draw() {
-    time += 1;
+    var now = new Date();
+    timePrev = time;
+    time = now.getTime() / 1000 - startTime;
+    var timeStep = time - timePrev;
 
-    if (keys.right && player.maxX() < x + nominalWidth) player.x += 2;
-    if (keys.left && player.minX() > x) player.x -= 2;
-    if (keys.down && player.maxY() < y + nominalHeight) player.y += 2;
-    if (keys.up && player.minY() > y) player.y -= 2;
-    player.x += 1;
-    x += 1;
+    var playerSpeed = 60;
+    var canvasSpeed = 30;
+    var bulletSpeed = 50;
+
+    if (alive) {
+	if (keys.right && player.maxX() < x + nominalWidth)
+	    player.x += timeStep * playerSpeed;
+	if (keys.left && player.minX() > x)
+	    player.x -= timeStep * playerSpeed;
+	if (keys.up && player.maxY() < y + nominalHeight)
+	    player.y += timeStep * playerSpeed;
+	if (keys.down && player.minY() > y)
+	    player.y -= timeStep * playerSpeed;
+	player.y += timeStep * canvasSpeed;
+	y += timeStep * canvasSpeed;
+    }
 
     context2D.clearRect(0, 0, canvas.width, canvas.height);
 
+    var aliveBullets = [];
+    for (var i = 0 ; i < bullets.length ; i++) {
+	bullets[i].move(timeStep);
+	if (alive) bullets[i].y += timeStep * canvasSpeed;
+	if (!bullets[i].isDead()) {
+	    aliveBullets.push(bullets[i]);
+	}
+	if ((bullets[i].x - player.x) * (bullets[i].x - player.x) +
+	    (bullets[i].y - player.y) * (bullets[i].y - player.y)
+	    < (14 + 10) * (14 + 10))
+	{
+	    alive = false;
+	}
+    }
+    bullets = aliveBullets;
+
+    startAlpha += Math.random() / 10;
+    if (alive && Math.random() > 0.9) {
+	var alpha;
+	for (alpha = startAlpha ; alpha < 2 * Math.PI + startAlpha ; alpha += Math.PI / 8) {
+	    bullets.push(new Bullet(redbullet,
+				    x + nominalWidth / 2,
+				    y + nominalHeight / 4 * 3,
+				    Math.cos(alpha) * bulletSpeed,
+				    Math.sin(alpha) * bulletSpeed,
+				    Math.cos(alpha) * bulletSpeed * 0.1,
+				    Math.sin(alpha) * bulletSpeed * 0.1));
+	}
+    }
+
+    var count = 0;
     var tile_x = Math.floor(x / image.width);
     var my_x = tile_x * image.width - x;
     while (my_x < nominalWidth) {
 	var tile_y = Math.floor(y / image.height);
 	var my_y = tile_y * image.height - y;
 	while (my_y < nominalHeight) {
-	    context2D.drawImage(image, my_x * scale, my_y * scale,
+	    context2D.drawImage(image, my_x * scale,
+				(nominalHeight - my_y - image.height) * scale,
 				image.width * scale, image.height * scale);
 	    my_y += image.height;
+	    count++;
 	}
 	my_x += image.width;
     }
+
     context2D.drawImage(player.image,
 			(player.minX() - x) * scale,
-			(player.minY() - y) * scale,
+			(nominalHeight - (player.minY() - y) - player.image.height) * scale,
 			player.image.width * scale,
 			player.image.height * scale);
-    context2D.fillText((time / 30).toFixed(2), 0, 10);
+
+    for (var i = 0 ; i < bullets.length ; ++i) {
+	var b = bullets[i];
+	context2D.drawImage(b.image,
+			    (b.minX() - x) * scale,
+			    (nominalHeight - (b.minY() - y) - b.image.height) * scale,
+			    b.image.width * scale, b.image.height * scale);
+    }
+    context2D.fillText(time.toFixed(2) + " " + count, 0, 10);
 }
