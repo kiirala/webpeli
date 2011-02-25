7ed01f6448345e6288f36ad8c2c587cae59d84d5
kiirala <kiirala@15d524df-0ca0-45ad-9872-4ba5b019094f>
2011-02-25 15:29:55 +0000

    Lisätty webpeliin vihollinen ja ennalta määritelty ammussarja
    git-svn-id: svn+ssh://melkki/fs-1/1/kiirala/repo-svn/webpeli@101 15d524df-0ca0-45ad-9872-4ba5b019094f

--- draw.js
+++ draw.js
@@ -1,5 +1,8 @@
 // target frames per second
 const FPS = 30;
+var playerSpeed = 60;
+var canvasSpeed = 30;
+var bulletSpeed = 50;
 var x = 0;
 var y = 0;
 var time = 0;
@@ -51,10 +54,76 @@ Bullet.prototype.move = function(time) {
 }
 Bullet.prototype.isDead = function() {
     if (this.maxX() < x || this.minX() >= x + nominalWidth ||
-	this.maxY() < y || this.maxY() >= y + nominalHeight)
+	this.maxY() < y || this.minY() >= y + nominalHeight)
 	return true;
     else
 	return false;
+}
+
+function TestEnemy(image, x, y) {
+    this.image = image;
+    this.x = x;
+    this.y = y;
+    this.time = 0;
+    this.seqPos = 0;	
+}
+TestEnemy.prototype = new Sprite();
+TestEnemy.prototype.sequence = [
+    [1.0, shootCircle, 16, 0],
+    [0.4, shootCircle, 16, 0],
+    [0.4, shootCircle, 16, 0],
+
+    [1.0, shootCircle, 16, 0],
+    [0.4, shootCircle, 16, 0.1],
+    [0.4, shootCircle, 16, 0.2],
+    [0.4, shootCircle, 16, 0.3],
+    [0.4, shootCircle, 16, 0.4],
+
+    [0.4, shootCircle, 16, 0.5],
+    [0.4, shootCircle, 16, 0.4],
+    [0.4, shootCircle, 16, 0.3],
+    [0.4, shootCircle, 16, 0.2],
+    [0.4, shootCircle, 16, 0.1],
+
+];
+
+TestEnemy.prototype.update = function(time) {
+    this.time += time;
+    if (this.time >= this.sequence[this.seqPos][0]) {
+	this.sequence[this.seqPos][1](this.x, this.y, this.sequence[this.seqPos][2], this.sequence[this.seqPos][3]);
+	this.time -= this.sequence[this.seqPos][0];
+	this.seqPos++;
+	if (this.seqPos >= this.sequence.length) {
+	    this.seqPos = 0;
+	}
+    }
+}
+
+function addBullet(bullet) {
+    if (bullets.used == bullets.length) {
+	bullets.push(bullet);
+    }
+    else {
+	bullets[bullets.used] = bullet;
+    }
+    bullets.used++;
+}
+function delBullet(index) {
+    bullets.used--;
+    bullets[index] = bullets[bullets.used];
+}
+
+function shootCircle(x, y, count, start) {
+    for (var alpha = start ;
+	 alpha < 2 * Math.PI + start ;
+	 alpha += Math.PI / (count / 2)) {
+	addBullet(new Bullet(redbullet,
+			     x, y,
+			     Math.cos(alpha) * bulletSpeed,
+			     Math.sin(alpha) * bulletSpeed,
+			     Math.cos(alpha) * bulletSpeed * 0.1,
+			     Math.sin(alpha) * bulletSpeed * 0.1));
+    }
 }
 
 function getWindowSize() {
@@ -77,6 +146,7 @@ function getWindowSize() {
 
 function resizeWindow() {
     var size = getWindowSize();
+    size.height = Math.min(nominalHeight, size.height);
     if (size.width / aspectRatio < size.height) {
 	canvas.width = size.width;
 	canvas.height = size.width / aspectRatio;
@@ -89,18 +159,22 @@ function resizeWindow() {
 }
 
 function initGame() {
+    var now = new Date();
+    startTime = now.getTime() / 1000;
+    time = 0;
+    timeAlive = 0;
     bullets = [];
+    bullets.used = 0;
     player = new Sprite(plimg);
     player.x = nominalWidth / 2;
     player.y = player.image.height / 2;
     x = 0;
     y = 0;
-    var now = new Date();
-    startTime = now.getTime() / 1000;
     startAlpha = 0;
     alive = true;
     graze = 0;
-    timeAlive = 0;
+    enemies = [];
+    enemies.push(new TestEnemy(badguy, nominalWidth / 2, y + nominalHeight / 4 * 3));
 }
 
 var imagesLoaded = 0;
@@ -118,22 +192,23 @@ function loadingDone() {
 $(function() {
     canvas = document.getElementById('canvas');
     context2D = canvas.getContext('2d');
+
     resizeWindow();
-    imagesToLoad = 2;
+
+    imagesToLoad = 3;
     plimg = new Image();
     plimg.src = "character.png"
     $(plimg).load(loadingDone);
     redbullet = new Image();
     redbullet.src = "redbullet.png";
     $(redbullet).load(loadingDone);
+    badguy = new Image();
+    badguy.src = "badguy.png";
+    $(badguy).load(loadingDone);
+    $("#restart").click(initGame);
 });
     
-$(window).resize(
-function() {
-    resizeWindow();
-});
-
-$("#restart").click(initGame);
+$(window).resize(resizeWindow);
 
 var keys = [];
 keys.down = false;
@@ -187,59 +262,50 @@ function draw() {
     timePrev = time;
     time = now.getTime() / 1000 - startTime;
     var timeStep = time - timePrev;
-    if (alive) timeAlive = time;
 
-    var playerSpeed = 60;
-    var canvasSpeed = 30;
-    var bulletSpeed = 50;
+    if (!alive) return;
 
-    if (alive) {
-	if (keys.right && player.maxX() < x + nominalWidth)
-	    player.x += timeStep * playerSpeed;
-	if (keys.left && player.minX() > x)
-	    player.x -= timeStep * playerSpeed;
-	if (keys.up && player.maxY() < y + nominalHeight)
-	    player.y += timeStep * playerSpeed;
-	if (keys.down && player.minY() > y)
-	    player.y -= timeStep * playerSpeed;
-	player.y += timeStep * canvasSpeed;
-	y += timeStep * canvasSpeed;
+    timeAlive = time;
+    if (keys.right && player.maxX() < x + nominalWidth)
+	player.x += timeStep * playerSpeed;
+    if (keys.left && player.minX() > x)
+	player.x -= timeStep * playerSpeed;
+    if (keys.up && player.maxY() < y + nominalHeight)
+	player.y += timeStep * playerSpeed;
+    if (keys.down && player.minY() > y)
+	player.y -= timeStep * playerSpeed;
+    
+    y += timeStep * canvasSpeed;
+    player.y += timeStep * canvasSpeed;
+    for (var i = 0 ; i < enemies.length ; ++i)
+	enemies[i].y += timeStep * canvasSpeed;
+    for (var i = 0 ; i < enemies.length ; i++) {
+	enemies[i].update(timeStep);
     }
-
-    context2D.clearRect(0, 0, canvas.width, canvas.height);
-
-    var aliveBullets = [];
-    for (var i = 0 ; i < bullets.length ; i++) {
+    
+    var i = 0;
+    while (i < bullets.used) {
 	bullets[i].move(timeStep);
 	if (alive) bullets[i].y += timeStep * canvasSpeed;
-	if (!bullets[i].isDead()) {
-	    aliveBullets.push(bullets[i]);
+	if (bullets[i].isDead()) {
+	    delBullet(i);
+	    i--;
 	}
 	var dist = (bullets[i].x - player.x) * (bullets[i].x - player.x) +
 	    (bullets[i].y - player.y) * (bullets[i].y - player.y);
 	if (dist < (14 + 10) * (14 + 10)) {
 	    alive = false;
+	    delBullet(i);
+	    i--;
 	}
 	else if (alive && dist < (14 + 32) * (14 + 32) && !bullets[i].grazed) {
 	    graze++;
 	    bullets[i].grazed = true;
 	}
+	i++;
     }
-    bullets = aliveBullets;
 
-    startAlpha += Math.random() / 10;
-    if (alive && Math.random() > 0.95) {
-	var alpha;
-	for (alpha = startAlpha ; alpha < 2 * Math.PI + startAlpha ; alpha += Math.PI / 8) {
-	    bullets.push(new Bullet(redbullet,
-				    x + nominalWidth / 2,
-				    y + nominalHeight / 4 * 3,
-				    Math.cos(alpha) * bulletSpeed,
-				    Math.sin(alpha) * bulletSpeed,
-				    Math.cos(alpha) * bulletSpeed * 0.1,
-				    Math.sin(alpha) * bulletSpeed * 0.1));
-	}
-    }
+    context2D.clearRect(0, 0, canvas.width, canvas.height);
 
     var count = 0;
     var tile_x = Math.floor(x / image.width);
@@ -263,6 +329,15 @@ function draw() {
 			player.image.width * scale,
 			player.image.height * scale);
 
+
+    for (var i = 0 ; i < enemies.length ; ++i) {
+	var e = enemies[i];
+	context2D.drawImage(e.image,
+			    (e.minX() - x) * scale,
+			    (nominalHeight - (e.minY() - y) - e.image.height) * scale,
+			    e.image.width * scale, e.image.height * scale);
+    }
+
     for (var i = 0 ; i < bullets.length ; ++i) {
 	var b = bullets[i];
 	context2D.drawImage(b.image,
@@ -270,6 +345,7 @@ function draw() {
 			    (nominalHeight - (b.minY() - y) - b.image.height) * scale,
 			    b.image.width * scale, b.image.height * scale);
     }
+
     //context2D.fillText(time.toFixed(2) + " " + count + " " + graze, 0, 10);
     $("#score").html((timeAlive * 1000 + graze * 10000).toFixed(0));
     $("#timeAlive").html(timeAlive.toFixed(1) + ' s');
